1da0be74fa5243a9b3967568cd7e4c9dce5d0062
Tyler RC <tylrrc@gmail.com>
2019-05-12 19:36:05 -0500

    Still a catastrophic mess

--- team21_project3.py
+++ team21_project3.py
@@ -45,18 +45,26 @@ PreIssueBuffer = Queue(maxsize=4)
 PostALUBuffer = None
 PostMEMBuffer = None
 
-#Cache components
+# Cache components
 hash = {}
 item_list = []
 
 cache = None
+totalCycles = len(opcode) + 4
+cycleNum = 1
+instQueue = Queue(maxsize=len(opcode))
+
+GenIter = 0
+
+
 
 
 def simulate_regs(inst):
-    arg1 = inst[1]
-    arg2 = inst[2]
-    arg3 = inst[3]
-    opcode = inst[4]
+    opcode = inst.opcode
+    arg1 = inst.arg1
+    arg2 = inst.arg2
+    arg3 = inst.arg3
+
     if int(opcode, base=2) == 1112:  # ADD
         regs[arg3] = regs[arg1] + regs[arg2]
 
@@ -96,9 +104,9 @@ def simulate_regs(inst):
     elif 1940 <= int(opcode, base=2) <= 1943:  # MOVK
         regs[arg3] = regs[arg3] + (arg1 << arg2)
 
-    #elif int(opcode, base=2) == 1986:  # LDUR
-        #regs[arg3] = data[arg2 - offset]
-        #more special things
+    # elif int(opcode, base=2) == 1986:  # LDUR
+    # regs[arg3] = data[arg2 - offset]
+    # more special things
 
     elif int(opcode, base=2) == 1984:  # STUR
         while len(data) < arg2:
@@ -121,205 +129,143 @@ def simulate_regs(inst):
     elif int(opcode, base=2) == 1692:  # ASR
         regs[arg3] = regs[arg1] >> arg2
 
+
 class WriteBackUnit:
     def run(self):
-        print ("INSIDE WriteBackUnit")
-        print ("Writing back " + str(PostALUBuffer) + " and " + str(PostMEMBuffer))
+        #print ("INSIDE WRITEBACKUNIT")
+        global PostALUBuffer
         simulate_regs(PostALUBuffer)
+        PostALUBuffer = None
+        #need to write back to registers
 
 class ALUnit:
     def run(self):
         global PostALUBuffer
-        inst = PreALUQueue.get()
-        print ("INSIDE ALUnit")
-        print ("ALU fetched " + str(inst))
-        PostALUBuffer = inst
-
+        PostALUBuffer = (PreALUQueue.get())
 
 class MEMUnit:
     def run(self):
-        global PostMEMBuffer
-        print ("INSIDE MEMUnit")
-        if not PreMEMQueue.empty():
-            PostMEMBuffer = PreMEMQueue.get()
-            print ("MEM now contains " + PostMEMBuffer)
-        else:
-            print ("No PreMEM")
-
+        #print ("INSIDE MEMUnit")
+        pass
 
 class IssueUnit:
     def run(self):
+        #print ("INSIDE ISSUEUNIT")
         inst1 = PreIssueBuffer.get()
         inst2 = PreIssueBuffer.get()
-        print ("INSIDE IssueUnit")
-        print ("IssueUnit contains ") + str(inst1) + " and " + str(inst2)
         if inst1 in ("LDUR", "STUR", "B", "CBZ", "CBNZ"):
-            PreMEMQueue.put(inst1)
-            print ("inst1 to PreMEMQueue")
+            if not PreMEMQueue.full():
+                PreMEMQueue.put(inst1)
         else:
-            PreALUQueue.put(inst1)
-            print ("inst1 to PreALUQueue")
+            if not PreALUQueue.full():
+                PreALUQueue.put(inst1)
         if inst2 in ("LDUR", "STUR", "B", "CBZ", "CBNZ"):
-            PreMEMQueue.put(inst2)
-            print ("inst2 to PreMEMQueue")
+            if not PreMEMQueue.full():
+                PreMEMQueue.put(inst2)
         else:
-            PreALUQueue.put(inst2)
-            print ("inst2 to PreALUQueue")
+            if not PreALUQueue.full():
+                PreALUQueue.put(inst2)
 
 
 class InstrFetch:
     # eventually needs to fetch from cache
     # getting instruction directly from instruction list for now
-    def __init__(self):
-        self.i = 0
-
-    def run(self, cache):
-        print ("INSIDE InstrFetch")
-        print ("Fetching " + opcode_str[self.i] + " and " + opcode_str[self.i + 1])
-        print ("and sending them to PreIssueBuffer")
-        PreIssueBuffer.put([opcode_str[self.i], arg1[self.i], arg2[self.i], arg3[self.i], opcode[self.i]])
-        PreIssueBuffer.put([opcode_str[self.i+1], arg1[self.i+1], arg2[self.i+1], arg3[self.i+1], opcode[self.i+1]])
-
-        print ("PreIssueBuffer now contains ")
-        for x in list(PreIssueBuffer.queue):
-            print (x)
-        # a = LRUCacheItem(1, raw_instruction[self.i])
-        # b = LRUCacheItem(2, raw_instruction[self.i+1])
-        # cache.insertItem(a)
-        # cache.insertItem(b)
-        self.i += 2
-
-        return True
-
-class LRUCacheItem(object):
-    """Data structure of items stored in cache"""
-    def __init__(self, key, item):
-        self.key = key
-        self.item = item
-        self.timestamp = datetime.now()
-
-class Cache:
-    # def __init__(self):
-    #     self.LRUBits = []
-    #     self.ValidBits = []
-    #     self.DirtyBits = []
-    #     self.Tag = []
-    #     self.Data = []
-    #     self.LRUBits[:4] = [0] * 4
-    #     self.ValidBits[:8] = [0] * 8
-    #     self.DirtyBits[:8] = [0] * 8
-    #     self.Tag[:8] = [0] * 8
-    #     self.Data[:8] = [0] * 8
-    #     cache = [self.LRUBits, self.ValidBits, self.DirtyBits, self.Tag, self.Data]
 
     def run(self):
-        pass
-
-    """A sample class that implements LRU algorithm"""
-
-    def __init__(self, length, delta=None):
-        self.length = length
-        self.delta = delta
-
-    def insertItem(self, item):
-        """Insert new items to cache"""
-
-        if item.key in hash:
-            # Move the existing item to the head of item_list.
-            item_index = item_list.index(item)
-            item_list[:] = item_list[:item_index] + item_list[item_index + 1:]
-            item_list.insert(0, item)
+        #print ("INSIDE INSTRFETCH")
+        if PreIssueBuffer.full() | instQueue.empty():
+            return
         else:
-            # Remove the last item if the length of cache exceeds the upper bound.
-            if len(item_list) > self.length:
-                self.removeItem(item_list[-1])
-
-            # If this is a new item, just append it to
-            # the front of item_list.
-            hash[item.key] = item
-            item_list.insert(0, item)
-
-    def removeItem(self, item):
-        """Remove those invalid items"""
-
-        del hash[item.key]
-        del item_list[item_list.index(item)]
-
-    def validateItem(self):
-        """Check if the items are still valid."""
-
-        def _outdated_items():
-            now = datetime.now()
-            for item in item_list:
-                time_delta = now - item.timestamp
-                if time_delta.seconds > self.delta:
-                    yield item
-
-        map(lambda x: self.removeItem(x), _outdated_items())
+            try:
+                PreIssueBuffer.put(instQueue.get())
+                #fix this / should determine how much to "put" dynamically
+            except:
+                return
+            try:
+                PreIssueBuffer.put(instQueue.get())
+            except:
+                return
 
 class Processor:
-    def __init__(self, instrs, opcodes, mem, valids, addrs, args1, args2, args3, numInstrs, dest, src1, src2):
-        self.instruction = instrs
-        self.opcode = opcodes
-        self.memory = mem
-        self.address = addrs
-        self.numInstructions = numInstrs
-        self.arg1 = arg1
-        self.arg2 = arg2
-        self.arg3 = arg3
-        self.destReg = dest
-        self.src1Reg = src1
-        self.src2Reg = src2
-        self.cycle = 1
+    def __init__(self):
         self.WB = WriteBackUnit()
         self.ALU = ALUnit()
         self.MEM = MEMUnit()
         self.issue = IssueUnit()
         self.fetch = InstrFetch()
-        self.cache = Cache(length=8, delta=5)
-
-        self.run()
+        #self.cache = Cache(length=8, delta=5)
 
     def run(self):
-        go = True;
-        while go:
+        global cycleNum
+        while cycleNum < 5*len(opcode):
+
+            self.fetch.run()
+            cycleNum += 1
             self.printState()
-            go = self.fetch.run(self.cache)
+
+            # print ("PREISSUEBUFFER CONTENTS: ")
+            # for x in list(PreIssueBuffer.queue):
+            #     print x.getInstStr()
+
             self.issue.run()
+            cycleNum += 1
+            self.printState()
+
+
+
+            # print ("PREISSUEBUFFER CONTENTS: ")
+            # for x in list(PreIssueBuffer.queue):
+            #     print x.getInstStr()
+
+            self.printState()
+            self.fetch.run()
             self.ALU.run()
+
+            # print ("PREISSUEBUFFER CONTENTS: ")
+            # for x in list(PreIssueBuffer.queue):
+            #     print x.getInstStr()
+
             self.MEM.run()
+            cycleNum += 1
+
+            # print ("PREISSUEBUFFER CONTENTS: ")
+            # for x in list(PreIssueBuffer.queue):
+            #     print x.getInstStr()
+
+            self.printState()
             self.WB.run()
-            self.cycle += 1
-    #     # jury-rigging. This goes away following further project implementation
-    #     self.cycle = self.numInstructions + 4
-    #
-    #
-    #
-    #     go = True
-    #     while go:
-    #         self.WB.run()
-    #         self.ALU.run()
-    #         self.MEM.run()
-    #         self.issue.run()
-    #         go = self.fetch.run(self.cache)
-    # #       self.printState()  # prints everything/ should only print once per cycle
-    #         self.cycle += 1
+
+            cycleNum += 1
+
+            # print ("PREISSUEBUFFER CONTENTS: ")
+            # for x in list(PreIssueBuffer.queue):
+            #     print x.getInstStr()
 
     def printState(self):
         print ("--------------------")
-        print ("Cycle: " + str(self.cycle))
+        print ("Cycle: " + str(cycleNum))
         print ("Pre-Issue Buffer:")
         for j in range(4):
-            print ("\tEntry " + str(j) + ":\t")
+            print ("\tEntry " + str(j) + ":\t"),
             try:
-                print list(PreIssueBuffer.queue[j])
+                print (PreIssueBuffer.queue[j].getInstStr())
             except:
+                print "\n"
                 continue
         print ("Pre_ALU Queue:")
         for j in range(2):
-            print ("\tEntry " + str(j) + ":")
+            print ("\tEntry " + str(j) + ":\t"),
+            try:
+                print (PreALUQueue.queue[j].getInstStr())
+            except:
+                print "\n"
+                continue
         print ("Post_ALU Queue:")
-        print ("\tEntry 0:")
+        print ("\tEntry 0:\t"),
+        try:
+            print (PostALUBuffer.getInstStr())
+        except:
+            pass
         print ("Pre_MEM Queue:")
         for j in range(2):
             print ("\tEntry " + str(j) + ":")
@@ -335,172 +281,24 @@ class Processor:
         print ("\nCache")
         for j in range(4):
             print ("Set " + str(j) + ":LRU=")
-            for k in range(2):  #you'll need to come back and fix this to print queues!!!!!!!!!!!
-                print ("\tEntry " + str(k) + ":[(" + "0" + "," + "0" + "," + "0" + ")<" + "," +">]")
+            for k in range(2):  # you'll need to come back and fix this to print queues!!!!!!!!!!!
+                print ("\tEntry " + str(k) + ":[(" + "0" + "," + "0" + "," + "0" + ")<" + "," + ">]")
         print ("\nData")
 
 
-# class Simulator:
-#
-#     def __init__(self, inFile, outFile):
-#
-#         self.input_file_name = inFile
-#         self.output_file_name = outFile
-#         self.input_file = open(str(self.input_file_name))
-#         self.outfile = open(self.output_file_name + "_sim.txt", 'w')
-#         self.simulate_regs()
-#
-#     def simulate_regs(self):
-#
-#         cycleCount = 1
-#         i = 0
-#
-#         while i < (len(opcode)):
-#             if int(opcode[i], base=2) == 1112:  # ADD
-#                 regs[arg3[i]] = regs[arg1[i]] + regs[arg2[i]]
-#
-#             elif int(opcode[i], base=2) == 1624:  # SUB
-#                 regs[arg3[i]] = regs[arg1[i]] - regs[arg2[i]]
-#
-#             elif int(opcode[i], base=2) == 1104:  # AND
-#                 regs[arg3[i]] = regs[arg1[i]] & regs[arg2[i]]
-#
-#             elif int(opcode[i], base=2) == 1360:  # ORR
-#                 regs[arg3[i]] = regs[arg1[i]] | regs[arg2[i]]
-#
-#             elif 160 <= int(opcode[i], base=2) <= 191:  # B
-#                 # self.print_lists(i, cycleCount)
-#                 self.out_sim_to_file(i, cycleCount)
-#                 i = i + arg1[i]
-#                 cycleCount += 1
-#                 continue
-#
-#             elif int(opcode[i], base=2) in (1160, 1161):  # ADDI
-#                 regs[arg3[i]] = regs[arg1[i]] + arg2[i]
-#
-#             elif int(opcode[i], base=2) in (1672, 1673):  # SUBI
-#                 regs[arg3[i]] = regs[arg1[i]] - arg2[i]
-#
-#             elif 1440 <= int(opcode[i], base=2) <= 1447:  # CBZ
-#                 if regs[arg3[i]] == 0:
-#                     # self.print_lists(i, cycleCount)
-#                     self.out_sim_to_file(i, cycleCount)
-#                     i = i + arg1[i]
-#                     cycleCount += 1
-#                     continue
-#
-#             elif 1448 <= int(opcode[i], base=2) <= 1455:  # CBNZ
-#                 if regs[arg3[i]] != 0:
-#                     # self.print_lists(i, cycleCount)
-#                     self.out_sim_to_file(i, cycleCount)
-#                     i = i + arg1[i]
-#                     cycleCount += 1
-#                     continue
-#
-#             elif 1684 <= int(opcode[i], base=2) <= 1687:  # MOVZ
-#                 regs[arg3[i]] = 0
-#                 regs[arg3[i]] = arg1[i] << arg2[i]
-#
-#             elif 1940 <= int(opcode[i], base=2) <= 1943:  # MOVK
-#                 regs[arg3[i]] = regs[arg3[i]] + (arg1[i] << arg2[i])
-#
-#             elif int(opcode[i], base=2) == 1986:  # LDUR
-#                 regs[arg3[i]] = data[arg2[i] - offset]
-#
-#             elif int(opcode[i], base=2) == 1984:  # STUR
-#                 while len(data) < arg2[i]:
-#                     data.extend(dataExt)
-#
-#                 base = regs[arg3[i]]
-#                 offset = ((addr[-1] + 4) - base) / 4
-#
-#                 data[arg2[i] - offset] = regs[arg3[i]]
-#
-#             elif int(opcode[i], base=2) == 1872:  # EOR
-#                 regs[arg3[i]] = regs[arg1[i]] ^ regs[arg2[i]]
-#
-#             elif int(opcode[i], base=2) == 1690:  # LSR
-#                 regs[arg3[i]] = regs[arg1[i]] >> arg2[i]
-#
-#             elif int(opcode[i], base=2) == 1691:  # LSL
-#                 regs[arg3[i]] = regs[arg1[i]] << arg2[i]
-#
-#             elif int(opcode[i], base=2) == 1692:  # ASR
-#                 regs[arg3[i]] = regs[arg1[i]] >> arg2[i]
-#
-#             # elif int(opcode[i], base=2) == 0:         #NOP
-#
-#             # elif int(opcode[i], base=2) == 2038:      #BREAK
-#
-#             # Use this for debugging
-#             # self.print_lists(i, cycleCount)
-#
-#             # Commented out because simulator output file no longer needed for assignment
-#             # self.out_sim_to_file(i, cycleCount)
-#             cycleCount += 1
-#             i += 1
-#
-#     def print_lists(self, i, cycleCount):
-#         print ("====================\n")
-#         print ("cycle:" + str(cycleCount) + "\t" + str(addr[i]) + "\t" + str(opcode_str[i]) + " " + arg1Str[i] +
-#                arg2Str[
-#                    i] + arg3Str[i])
-#         print ("\n")
-#         print ("registers:\n")
-#         for j in range(32):
-#             if j % 8 == 0:
-#                 print "r" + str(j).zfill(2) + ":\t",
-#             print str(regs[j]) + "\t",
-#             if j % 8 == 7:
-#                 print ("\n"),
-#         print ("\n"),
-#         print ("data:")
-#
-#         line = 32
-#         if not data:
-#             return
-#         k = addr[-1] + 4
-#         for j in range(len(data)):
-#             if j % 8 == 0:
-#                 print str(k) + ":\t",
-#                 k += line
-#             print str(data[j]) + "\t",
-#             if j % 8 == 7:
-#                 print ("\n"),
-#
-#     def out_sim_to_file(self, i, cycleCount):
-#         self.outfile.write("=====================\n")
-#         self.outfile.write("cycle:" + str(cycleCount) + "\t" + str(addr[i]) + "\t" + str(opcode_str[i]))
-#         if arg1Str[i] != "" or arg2Str[i] != "":
-#             self.outfile.write("\t" + arg1Str[i] + arg2Str[i] + arg3Str[i])
-#         self.outfile.write("\n\n")
-#         self.outfile.write("registers:\n")
-#         for j in range(32):
-#             if j % 8 == 0:
-#                 self.outfile.write("r" + str(j).zfill(2) + ":\t")
-#             if j % 8 != 7:
-#                 self.outfile.write(str(regs[j]) + "\t")
-#             if j % 8 == 7:
-#                 self.outfile.write(str(regs[j]) + "\n")
-#         self.outfile.write("\n")
-#         if not data:
-#             self.outfile.write("data:" + "\n\n\n")
-#         else:
-#             self.outfile.write("data:" + "\n")
-#
-#         line = 32
-#         if not data:
-#             return
-#         k = addr[-1] + 4
-#         for j in range(len(data)):
-#             if j % 8 == 0:
-#                 self.outfile.write(str(k) + ":")
-#                 k += line
-#             if j % 8 != 7:
-#                 self.outfile.write(str(data[j]) + "\t")
-#             if j % 8 == 7:
-#                 self.outfile.write(str(data[j]) + "\n")
-
+class INST:
+    def __init__(self, instNum, opcode_str, arg1Str, arg2Str, arg3Str, opcode, arg1, arg2, arg3):
+        self.instNum = instNum
+        self.opcode_str = opcode_str
+        self.arg1Str = arg1Str
+        self.arg2Str = arg2Str
+        self.arg3Str = arg3Str
+        self.opcode = opcode
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.arg3 = arg3
+    def getInstStr(self):
+        return self.opcode_str + " " + self.arg1Str + self.arg2Str + self.arg3Str
 
 class Disassembler:
     # Constructor sets up dummy values, gets I/O files, adds and processes input, and prints disassembled code
@@ -511,8 +309,8 @@ class Disassembler:
         self.input_file = open(str(self.input_file_name))
         self.input_to_lists()
         self.print_lists()
-        #Simulator(self.input_file_name, self.output_file_name)
-        Processor(raw_instruction, opcode, None, None, addr, arg1, arg2, arg3, len(opcode), None, None, None)
+        # Simulator(self.input_file_name, self.output_file_name)
+        #INST(raw_instruction, opcode, None, None, addr, arg1, arg2, arg3, len(opcode), None, None, None)
 
     # get_io_params() sets input_file_name and output_file_name to the string values immediately after the -i and -o args
     # It also prints the names of the I/O files
@@ -715,6 +513,7 @@ class Disassembler:
                 break
             addr.append(addrBase)
             addrBase += 4
+            instQueue.put(INST(i, opcode_str[i], arg1Str[i], arg2Str[i], arg3Str[i], opcode[i], arg1[i], arg2[i], arg3[i]))
 
     # returns 2s complement of bin
     def unsigned2signed(self, bin, bitNum):
@@ -789,3 +588,6 @@ class Disassembler:
 
 if __name__ == "__main__":  # Only runs if program executed as script
     disassembler = Disassembler()
+    global processor
+    processor = Processor()
+    processor.run()
